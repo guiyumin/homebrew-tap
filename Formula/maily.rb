9807c716e0107a7da3011829e6c235f8b3998e2a
Yumin Gui <guiyumin@protonmail.com>
2026-01-13 14:00:32 -0800

    Add post_install step to stop old maily server on upgrade

--- Formula/maily.rb
+++ Formula/maily.rb
@@ -18,6 +18,24 @@ class Maily < Formula
     bin.install "maily"
   end
 
+  def post_install
+    # Stop any running maily server to avoid version mismatch after upgrade.
+    # The server will auto-start with the new version when user runs maily.
+    pid_path = Pathname.new("#{Dir.home}/.config/maily/server.pid")
+    sock_path = Pathname.new("#{Dir.home}/.config/maily/maily.sock")
+
+    if pid_path.exist?
+      pid = pid_path.read.split(":").first.to_i
+      if pid > 0
+        ohai "Stopping old maily server (PID #{pid})..."
+        Process.kill("TERM", pid)
+        sleep 0.5
+      end
+      pid_path.unlink rescue nil
+      sock_path.unlink rescue nil
+    end
+  end
+
   test do
     assert_match version.to_s, shell_output("#{bin}/maily --version")
   end
